deedb8f945a0c5402adc8164d19b1a526d764745
Hadi Sharghi <hsharghi@gmail.com>
2023-08-17 17:39:27 +0330

    add password to createUser add loginUser method to UserControiler

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -5,6 +5,7 @@ struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let userRoutes = routes.grouped("user")
         userRoutes.post("", use: createUser)
+        userRoutes.post("login", use: loginUser)
         userRoutes.get("", use: listUsers)
         //
         //        let todos = routes.grouped("todos")
@@ -18,18 +19,55 @@ struct UserController: RouteCollection {
     struct CreateUserRequest: Content {
         var name: String
         var username: String?
+        var password: String
     }
     
-    func createUser(req: Request) async throws -> User {
+    func createUser(req: Request) async throws -> User.PublicUser {
         let data = try req.content.decode(CreateUserRequest.self)
-        let user = User(name: data.name, username: data.username)
+        let usersWithSameNameCount = try await User.query(on: req.db)
+            .filter(\.$name == data.name)
+            .count()
+        
+        guard usersWithSameNameCount == 0 else {
+            throw AppError.duplicateName
+        }
+        
+        let user = User(name: data.name,
+                        username: data.username,
+                        password: try req.password.hash(data.password))
         try await user.save(on: req.db)
+        
+        return user.toPublic()
+    }
+    
+    struct LoginUseRequest: Content {
+        var name: String
+        var password: String
+    }
+    
 
-        return user
+    func loginUser(req: Request) async throws -> String {
+        let data = try req.content.decode(LoginUseRequest.self)
+        let foundUser = try await User.query(on: req.db)
+            .filter(\.$name == data.name)
+            .first()
+        guard let foundUser else {
+            throw AppError.invalidUserPassword
+        }
+        guard try req.password.verify(data.password, created: foundUser.password) else {
+            throw AppError.invalidUserPassword
+        }
+        
+        let tokenString = try req.password.hash(String(Int.random()))
+        let token = Token(userId: try foundUser.requireID(),
+                          token: tokenString,
+                          expiresAt: Date().addingTimeInterval(60 * 60))
+        try await token.save(on: req.db)
+        return tokenString
     }
     
     func listUsers(req: Request) async throws -> [User] {
         try await User.query(on: req.db).all()
     }
-
+    
 }
